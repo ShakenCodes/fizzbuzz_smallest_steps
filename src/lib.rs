aec956e5d6358199bacd46a251e8ee756a36e200
Phil Markgraf <philip.markgraf@bionautlabs.com>
2021-09-15 11:51:22 -0700

    Solved kata - should have commiteed after every pass result.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub fn fizzbuzz_to_thirty() -> String {
+    let mut result = String::new();
+    for i in 1..=30 {
+        result.push_str(&fizzbuzz(i));
+    };
+    result
+}
+fn fizzbuzz(n: u32) -> String {
+    match (n % 5, n % 3) {
+        (0, 0) => "fizzbuzz\n".to_string(),
+        (_, 0) => "fizz\n".to_string(),
+        (0, _) => "buzz\n".to_string(),
+        (_, _) => format!("{}\n", n),
     }
 }
--- tests/fizzbuzz_ut.rs
+++ tests/fizzbuzz_ut.rs
@@ -1,9 +1,39 @@
-use fizzbuzz;
-
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 3);
+    fn test_fizzbuzz_to_thirty() {
+        assert_eq!(
+            "1\n\
+             2\n\
+             fizz\n\
+             4\n\
+             buzz\n\
+             fizz\n\
+             7\n\
+             8\n\
+             fizz\n\
+             buzz\n\
+             11\n\
+             fizz\n\
+             13\n\
+             14\n\
+             fizzbuzz\n\
+             16\n\
+             17\n\
+             fizz\n\
+             19\n\
+             buzz\n\
+             fizz\n\
+             22\n\
+             23\n\
+             fizz\n\
+             buzz\n\
+             26\n\
+             fizz\n\
+             28\n\
+             29\n\
+             fizzbuzz\n\
+             ",
+            fizzbuzz::fizzbuzz_to_thirty());
     }
 }
